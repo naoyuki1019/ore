2c6043ea8f9ad00b6bb7e686a0184a4d02649767
naoyuki onishi <naoyuki.onishi@gmail.com>
2019-05-15 01:04:03 +0900

    fix

--- tests/ore_ftpupload.php
+++ tests/ore_ftpupload.php
@@ -8,7 +8,6 @@ $ftp->flush = true;
 $ftp->set_sh_dir($dir.'/tmp');
 $ftp->add_file($dir.'/tmp/upload1.txt', '/test/dir1/uploaded1.txt');
 $ftp->add_file($dir.'/tmp/upload2.txt', '/test/dir2/uploaded2.txt');
-$ftp->add_file($dir.'/tmp/upload2.txt', '/aaaatest/dir2/uploaded2.txt');
 $ftp->host = 'ホスト名';
 $ftp->uid = 'ユーザー名';
 $ftp->pass = 'パスワード';

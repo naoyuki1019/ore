9b8c8d414d876ed38300577795515d7ab2628623
naoyuki onishi <naoyuki.onishi@gmail.com>
2021-05-04 19:53:23 +0900

    fix

--- application/core/ORE_Functions.php
+++ application/core/ORE_Functions.php
@@ -110,18 +110,22 @@ if (! function_exists('generateTreeName')) {
 			if (TRUE === $is_array) {
 				$r = (object)$r;
 			}
-			$depth = $r->{$vo->tree_depth};
-			$parent_id = $r->{$vo->tree_parent_id};
 
-			$last[$parent_id] = $r->{$vo->tree_key};
+			if (! isNULL($r, $vo->tree_depth) AND ! isNULL($r, $vo->tree_parent_id)) {
+				$depth = $r->{$vo->tree_depth};
+				$parent_id = $r->{$vo->tree_parent_id};
 
-			if (is_null($min_depth)) {
-				$min_depth = $depth;
+				$last[$parent_id] = $r->{$vo->tree_key};
+
+				if (is_null($min_depth)) {
+					$min_depth = $depth;
+				}
+
+				if ($min_depth > $depth) {
+					$min_depth = $depth;
+				}
 			}
 
-			if ($min_depth > $depth) {
-				$min_depth = $depth;
-			}
 			if (TRUE === $is_array) {
 				$r = (array)$r;
 			}
@@ -132,41 +136,44 @@ if (! function_exists('generateTreeName')) {
 			if (TRUE === $is_array) {
 				$r = (object)$r;
 			}
-			$depth = $r->{$vo->tree_depth};
-			$parent_id = $r->{$vo->tree_parent_id};
 
-			// 子供が存在する場合で且つ最小のdepthではない
-			if (isset($last[$parent_id]) AND $min_depth != $depth) {
-				// 最後の場合
-				if ($last[$parent_id] === $r->{$vo->tree_key}) {
-					$prefix[$depth] = '└─';
+			if (! isNULL($r, $vo->tree_depth) AND ! isNULL($r, $vo->tree_parent_id)) {
+				$depth = $r->{$vo->tree_depth};
+				$parent_id = $r->{$vo->tree_parent_id};
+
+				// 子供が存在する場合で且つ最小のdepthではない
+				if (isset($last[$parent_id]) AND $min_depth != $depth) {
+					// 最後の場合
+					if ($last[$parent_id] === $r->{$vo->tree_key}) {
+						$prefix[$depth] = '└─';
+					}
+					else {
+						$prefix[$depth] = '├─';
+					}
 				}
-				else {
-					$prefix[$depth] = '├─';
+
+				$tree_nm = '';
+
+				for ($i = 0; $i <= $depth; $i++) {
+					if (array_key_exists($i, $prefix)) {
+						$tree_nm .= $prefix[$i];
+					}
 				}
+
+				if (array_key_exists($depth, $prefix)) {
+					// 一回限りで変更
+					if ($prefix[$depth] == '├─') {
+						$prefix[$depth] = '│　';
+					}
+
+					// 一回限りで変更
+					if ($prefix[$depth] === '└─') {
+						$prefix[$depth] = '　　';
+					}
+				}
+
+				$r->tree_nm = $tree_nm.$r->{$vo->tree_label};
 			}
-
-			$tree_nm = '';
-
-			for ($i = 0; $i <= $depth; $i++) {
-				if (array_key_exists($i, $prefix)) {
-					$tree_nm .= $prefix[$i];
-				}
-			}
-
-			if (array_key_exists($depth, $prefix)) {
-				// 一回限りで変更
-				if ($prefix[$depth] == '├─') {
-					$prefix[$depth] = '│　';
-				}
-
-				// 一回限りで変更
-				if ($prefix[$depth] === '└─') {
-					$prefix[$depth] = '　　';
-				}
-			}
-
-			$r->tree_nm = $tree_nm.$r->{$vo->tree_label};
 
 			if (TRUE === $is_array) {
 				$r = (array)$r;
